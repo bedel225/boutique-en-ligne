4302b9a89a456e76ef8df7c3027f31a842960b98
bedel225 <abdoulchoco@yahoo.fr>
2024-10-06 13:22:29 +0200

    register controller

--- src/Form/RegisterUserType.php
+++ src/Form/RegisterUserType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -22,11 +23,21 @@ class RegisterUserType extends AbstractType
                     'placeholder' => 'indiquer votre adresse email'
                 ]
             ])
-            ->add('password', PasswordType::class, [
-                'label' => 'Votre mot de passe',
-                'attr' => [
-                    'placeholder' => 'indiquer votre mot de passe'
-                ]
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'label' => 'Votre mot de passe',
+                    'attr' => [
+                        'placeholder' => 'indiquer votre mot de passe'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'Confirmer votre mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Confirmer votre mot de passe'
+                    ]
+                ],
+                'mapped' => false,
             ])
             ->add('firstname', TextType::class,[
                 'label' => 'Votre prenom',
