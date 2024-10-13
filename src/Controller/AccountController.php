9d0bd446eb88211f43ee120f726cf93382449796
bedel225 <abdoulchoco@yahoo.fr>
2024-10-13 22:25:01 +0200

    modification du mot de passe

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\ModifierPwdType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -12,6 +15,23 @@ class AccountController extends AbstractController
     public function index(): Response
     {
         return $this->render('account/index.html.twig', [
+        ]);
+    }
+
+    #[Route('/modifier_pwd', name: 'app_modifier_pwd')]
+    public function modifier(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(ModifierPwdType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager->flush();
+        }
+        return $this->render('account/modifier_pawd.html.twig', [
+            'modifierPwdForm' => $form->createView(),
         ]);
     }
 }
