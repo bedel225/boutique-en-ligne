ae0a1e3e1143671bd54de36df36a6734a0f04d13
bedel225 <abdoulchoco@yahoo.fr>
2024-11-06 15:12:17 +0000

    panier

---
+++ src/Classe/Cart.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Classe;
+
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class Cart
+{
+    public function __construct(
+        private RequestStack $requestStack,
+    ) {
+        // $this->session = $requestStack->getSession();
+    }
+    public function getCart()
+    {
+        return $this->requestStack->getSession()->get('cart');
+    }
+    public function add($product)
+    {
+        $cart = $this->requestStack->getSession()->get('cart');
+        if (isset($cart[$product->getId()])) {
+            $cart[$product->getId()] = [
+                'object' => $product,
+                'qty' => $cart[$product->getId()]['qty'] +1,
+            ];
+        }else {
+            $cart[$product->getId()] = [
+                'object' => $product,
+                'qty' => 1,
+            ];
+        }
+
+        $this->requestStack->getSession()->set('cart', $cart);
+    }
+}
